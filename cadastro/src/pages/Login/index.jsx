d609992d96467c9c89f0c92b4dcbf4336fc51072
jaovw <joojvw@gmail.com>
2022-10-25 20:41:11 -0300

    utilizando o novo componente layout

--- cadastro/src/pages/Login/index.jsx
+++ cadastro/src/pages/Login/index.jsx
@@ -2,6 +2,7 @@ import { Link } from 'react-router-dom'
 import { useState } from 'react';
 import logo from '../../assets/logo.svg'
 import '../../styles.css'
+import { Layout } from '../../components/layout';
 
 export const Login = () => {
 
@@ -9,51 +10,45 @@ export const Login = () => {
     const [password, setPassword] = useState('')
 
     return (
-        <div className="container">
-            <div className="container-login">
-                <div className="wrap-login">
-                    <form className="login-form">
+        <Layout>
+            <form className="login-form">
 
-                        <span className="login-form-title">
-                            <img src={logo} alt='Logo'></img>
-                        </span>
+                <span className="login-form-title">
+                    <img src={logo} alt='Logo'></img>
+                </span>
 
-                        <span className="login-form-title">Bem vindo!</span>
+                <span className="login-form-title">Bem vindo!</span>
 
-                        <div className='wrap-input'>
-                            <input
-                                type='email'
-                                className={email !== '' ? 'has-val input' : 'input'}
-                                value={email}
-                                onChange={e => setEmail(e.target.value)}
-                            />
-                            <span className='focus-input' data-placeholder='Email'></span>
-                        </div>
-
-                        <div className='wrap-input'>
-                            <input
-                                type='password'
-                                className={password !== '' ? 'has-val input' : 'input'}
-                                value={password}
-                                onChange={e => setPassword(e.target.value)}
-                            />
-                            <span className='focus-input' data-placeholder='Password'></span>
-                        </div>
-
-                        <div className='container-login-form-btn'>
-                            <button className='login-form-btn'>Login</button>
-                        </div>
-
-                        <div className='text-center'>
-                            <span className='txt1'>Nao possui conta?</span>
-                            <Link className='txt2' to='#'>Criar conta.</Link>
-                        </div>
-
-                    </form>
+                <div className='wrap-input'>
+                    <input
+                        type='email'
+                        className={email !== '' ? 'has-val input' : 'input'}
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
+                    />
+                    <span className='focus-input' data-placeholder='Email'></span>
                 </div>
 
-            </div>
-        </div>
+                <div className='wrap-input'>
+                    <input
+                        type='password'
+                        className={password !== '' ? 'has-val input' : 'input'}
+                        value={password}
+                        onChange={e => setPassword(e.target.value)}
+                    />
+                    <span className='focus-input' data-placeholder='Password'></span>
+                </div>
 
+                <div className='container-login-form-btn'>
+                    <button className='login-form-btn'>Login</button>
+                </div>
+
+                <div className='text-center'>
+                    <span className='txt1'>Nao possui conta?</span>
+                    <Link className='txt2' to='#'>Criar conta.</Link>
+                </div>
+
+            </form>
+        </Layout>
     )
 }
